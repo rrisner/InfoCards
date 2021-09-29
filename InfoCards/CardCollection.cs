466f3b0e87f96a97456ed1380acaea1060fab538
Lenovo ThinkPad <Lenovo ThinkPad@DESKTOP-BC3UN1T>
2021-09-29 11:18:57 -0400

    Add project files.

---
+++ InfoCards/CardCollection.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InfoCards
+{
+    class CardCollection
+    {
+        private static int count = 0;
+        private static string DEFAULT_TITLE = "Untitled InfoCard Collection";
+        private static string COLLECTION_HEADER_TEXT = "CARDCOLLECTION::";
+        private static string DEFAULT_FILE_NAME = "My InfoCard Deck.deck";
+        private static string DEFAULT_SAVE_DIRECTORY = "./";
+
+        private string absoluteID;
+        private int ID; //Relative for this session
+        private string title;
+        private string fileName;
+        private string directory;
+        private DateTime timeCreated;
+        private DateTime timeEdited;
+        private List<InfoCard> collection;
+
+        public CardCollection()
+        {
+            setTimeCreated(DateTime.Now);
+            setID(nextID());
+            generateAbsoluteID();
+            setTitle(DEFAULT_TITLE);
+            setDirectory(DEFAULT_SAVE_DIRECTORY);
+            setFileName(DEFAULT_FILE_NAME);
+            clear(); // new infocard collection list
+        }
+
+        private void setTimeCreated(DateTime toSet)
+        {
+            timeCreated = toSet;
+            setTimeEdited(toSet);
+        }
+        private DateTime getTimeCreated()
+        {
+            return timeCreated;
+        }
+        private void setTimeEdited(DateTime toSet)
+        {
+            timeEdited = toSet;
+        }
+        private void updateEditTime()
+        {
+            setTimeEdited(DateTime.Now);
+        }
+        private DateTime getTimeEdited()
+        {
+            return timeEdited;
+        }
+        private void generateAbsoluteID()
+        {
+            Console.WriteLine(DateTime.Now.ToString() + getID().ToString());
+        }
+        public string getAbsoluteID()
+        {
+            return absoluteID;
+        }
+        private void setAbsoluteID(string ID)
+        {
+            absoluteID = ID;
+        }
+        private void setID(int toSet)
+        {
+            ID = toSet;
+        }
+        private int getID()
+        {
+            return ID;
+        }
+        private static int nextID()
+        {
+            return ++count;
+        }
+        private static int cardsConstructed()
+        {
+            return count;
+        }
+        private void setFileName(string toSet)
+        {
+            fileName = toSet;
+        }
+        public string getFileName()
+        {
+            return fileName;
+        }
+        public void updateFileName(string toSet)
+        {
+            updateEditTime();
+            setFileName(toSet);
+        }
+        private void setDirectory(string toSet)
+        {
+            directory = toSet;
+        }
+        public string getDirectory()
+        {
+            return directory;
+        }
+        public void updateDirectory(string toSet)
+        {
+            updateEditTime();
+            setDirectory(toSet);
+        }
+
+        private void setTitle(string toSet)
+        {
+            title = toSet;
+        }
+        public void updateTitle(string toSet)
+        {
+            setTitle(toSet);
+            updateEditTime();
+        }
+        private string getTitle()
+        {
+            return title;
+        }
+
+        public int size()
+        {
+            return collection.Count;
+        }
+        public bool add(InfoCard toAdd)
+        {
+            collection.Add(toAdd);
+
+            if (collection.Count == 1)
+            {
+                collection[0].setDirectionToOrigin(InfoCard.DIRECTION_TO_ORIGIN.IS_ORIGIN);
+            }
+
+            return true;
+        }
+        public void clear()
+        {
+            collection = new List<InfoCard>();
+        }
+        public InfoCard cardAt(int index)
+        {
+            if (index >= 0 && index < size())
+            {
+                return collection[index];
+            }
+            else
+            {
+                throw new Exception("No InfoCard exists at index " + index.ToString() + "!");
+                //return NULL_CARD;
+            }
+        }
+        public InfoCard getCardByAbsID(string absID)
+        {
+            if (absID.Equals(InfoCard.NULL_CARD_STR))
+            {
+                return NULL_CARD;
+            }
+
+            for (int i = 0; i < collection.Count; i++)
+            {
+                if (collection[i].getAbsoluteID().Equals(absID))
+                {
+                    return collection[i];
+                }
+            }
+            throw new Exception("Referenced InfoCard " + absID + " does not exist in this collection!");
+            //return NULL_CARD;
+        }
+        public bool removeByAbsID(InfoCard toRemove)
+        {
+            if (collection.Contains(toRemove))
+            {
+                collection.Remove(toRemove);
+                return true;
+            }
+            throw new Exception("Specified card " + toRemove.getAbsoluteID() + " does not exist in this collection!");
+            //return false;
+        }
+
+        public override string ToString()
+        {
+            string toReturn = "";
+            toReturn += COLLECTION_HEADER_TEXT + "\n";
+            toReturn += getAbsoluteID() + "\n";
+
+            toReturn += getTimeCreated().ToString() + "\n";
+            toReturn += getTimeEdited().ToString() + "\n";
+            toReturn += getTitle() + "\n";
+
+            toReturn += size().ToString() + "\n";
+            
+            for (int i = 0; i < size(); i++)
+            {
+                toReturn += cardAt(i).ToString();
+            }
+
+            return toReturn;
+        }
+        private void FromFile(System.IO.StreamReader reader)
+        {
+            if (reader.ReadLine().Equals(COLLECTION_HEADER_TEXT))
+            {
+                Console.WriteLine("Valid data");
+            }
+            setAbsoluteID(reader.ReadLine());
+
+            setTimeCreated(DateTime.Parse(reader.ReadLine()));
+            setTimeEdited(DateTime.Parse(reader.ReadLine()));
+            setTitle(reader.ReadLine());
+
+            //Build Collection of card objects
+            int cardCount = int.Parse(reader.ReadLine());
+            for (int i = 0; i < cardCount; i++)
+            {
+                add(new InfoCard(reader));
+            }
+        }
+
+        public bool save()
+        {
+            System.IO.StreamWriter writer = new System.IO.StreamWriter(getDirectory() + getFileName());
+            writer.Write(ToString());
+            writer.Close();
+            return true;
+        }
+
+        public bool load()
+        {
+            System.IO.StreamReader writer = new System.IO.StreamReader(getDirectory() + getFileName());
+            clear(); // load as replacement
+            FromFile(writer);
+            writer.Close();
+            return true;
+        }
+
+        public static InfoCard NULL_CARD = new InfoCard(InfoCard.NULL_CARD_STR);
+
+    }
+}
---
+++ InfoCards/InfoCard.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InfoCards
+{
+    class InfoCard
+    {
+        private static int count = 0;
+        public static string NULL_CARD_STR = "NULL CARD";
+        private static string DEFAULT_TITLE = "Untitled Card";
+        private static string DEFAULT_BODY_TEXT = "Replace with your text!";
+        private static string INFOCARD_HEADER_TEXT = "INFOCARD::";
+        public enum CARD_INTERFACE
+        {
+            NEXT = 0,
+            PREVIOUS = 1,
+            WHY = 2,
+            HOW = 3,
+            RETURN = 4
+        }
+        public enum CARD_TYPE
+        {
+            DEFAULT,
+            BULLETIN,
+            DESCRIPTION,
+            NULL_CARD
+        }
+        public enum DIRECTION_TO_ORIGIN
+        {
+            IS_ORIGIN,
+            UNKNOWN,
+            NEXT,
+            PREVIOUS,
+            RETURN
+        }
+
+        private string absoluteID;
+        private int ID; //Relative for this session
+        private string title;
+        private string text;
+        private int lines;
+        private int level;
+        private DIRECTION_TO_ORIGIN directionToOrigin;
+        private DateTime timeCreated;
+        private DateTime timeEdited;
+        private List<string> linkedCards;
+        private CARD_TYPE cardType;
+
+        public InfoCard()
+        {
+            setTimeCreated(DateTime.Now);
+            setID(nextID());
+            generateAbsoluteID();
+            initializeLinkedCards();
+            setCardType(CARD_TYPE.DEFAULT);
+            setTitle(DEFAULT_TITLE);
+            setText(DEFAULT_BODY_TEXT);
+            setLevel(0);
+            setDirectionToOrigin(DIRECTION_TO_ORIGIN.UNKNOWN);
+        }
+
+        public InfoCard(System.IO.StreamReader streamReader)
+        {
+            setID(nextID());
+            FromFile(streamReader);
+        }
+        public InfoCard(string specialID)
+        {
+            setTimeCreated(DateTime.Now);
+            setID(nextID());
+            setAbsoluteID(specialID);
+            initializeLinkedCards();
+
+            if (specialID.Equals(NULL_CARD_STR))
+            {
+                setCardType(CARD_TYPE.NULL_CARD);
+                setTitle(NULL_CARD_STR);
+                setText(NULL_CARD_STR);
+            }
+            else
+            {
+                setCardType(CARD_TYPE.DEFAULT);
+                setTitle(DEFAULT_TITLE);
+                setText(DEFAULT_BODY_TEXT);
+            }
+            
+            
+        }
+
+        private void setTimeCreated(DateTime toSet)
+        {
+            timeCreated = toSet;
+            setTimeEdited(toSet);
+        }
+        public DateTime getTimeCreated()
+        {
+            return timeCreated;
+        }
+        private void setTimeEdited(DateTime toSet)
+        {
+            timeEdited = toSet;
+        }
+        private void updateEditTime()
+        {
+            setTimeEdited(DateTime.Now);
+        }
+        public DateTime getTimeEdited()
+        {
+            return timeEdited;
+        }
+        private void generateAbsoluteID()
+        {
+            absoluteID = DateTime.Now.ToString() + getID().ToString();
+            Console.WriteLine(DateTime.Now.ToString() + getID().ToString());
+        }
+        public string getAbsoluteID()
+        {
+            return absoluteID;
+        }
+        private void setAbsoluteID(string ID)
+        {
+            absoluteID = ID;
+        }
+        private void setID(int toSet)
+        {
+            ID = toSet;
+        }
+        private int getID()
+        {
+            return ID;
+        }
+        private static int nextID()
+        {
+            return ++count;
+        }
+        private static int cardsConstructed()
+        {
+            return count;
+        }
+        private void setLevel(int newLevel)
+        {
+            level = newLevel;
+        }
+        public int getLevel()
+        {
+            return level;
+        }
+        private void setText(string toSet)
+        {
+            int lines = 1;
+            text = toSet;
+            foreach (char every in text.ToCharArray())
+            {
+                if (every.Equals('\n'))
+                {
+                    lines++;
+                }
+            }
+            setLines(lines);
+        }
+        public void updateText(string toSet)
+        {
+            setText(toSet);
+            updateEditTime();
+        }
+        public string getText()
+        {
+            return text;
+        }
+        private void setTitle(string toSet)
+        {
+            title = toSet;
+        }
+        public void updateTitle(string toSet)
+        {
+            setTitle(toSet);
+            updateEditTime();
+        }
+        public string getTitle()
+        {
+            return title;
+        }
+        public void setDirectionToOrigin(DIRECTION_TO_ORIGIN toSet)
+        {
+            directionToOrigin = toSet;
+        }
+        public DIRECTION_TO_ORIGIN getDirectionToOrigin()
+        {
+            return directionToOrigin;
+        }
+        private void initializeLinkedCards()
+        {
+            linkedCards = new List<string>();
+            foreach (CARD_INTERFACE x in Enum.GetValues(typeof(CARD_INTERFACE)))
+            {
+                linkedCards.Add(NULL_CARD_STR);
+            }
+        }
+        private bool setLinkedCard(int index, string absID)
+        {
+            if (index >= 0 && index < Enum.GetValues(typeof(CARD_INTERFACE)).Length)
+            {
+                linkedCards.RemoveAt(index);
+                linkedCards.Insert(index, absID);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool updateLinkedCard(int index, string absID)
+        {
+            if (setLinkedCard(index, absID))
+            {
+                updateEditTime();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool updateLinkedCard(CARD_INTERFACE relation, string absID)
+        {
+            return updateLinkedCard((int)relation, absID);
+        }
+        public string getLinkedCardAbsID(int index)
+        {
+            if (index >= 0 && index < linkedCards.Count)
+            {
+                return linkedCards[index];
+            }
+            else
+            {
+                throw new Exception("Linked card does not exist for index " + index.ToString());
+                //return NULL_CARD_STR;
+            }
+
+        }
+        public string getLinkedCardAbsID(CARD_INTERFACE relation)
+        {
+            return getLinkedCardAbsID((int)relation);
+        }
+        public void setRelation(CARD_INTERFACE relation, InfoCard other)
+        {
+            if (this.getAbsoluteID().Equals(NULL_CARD_STR))
+            {
+                //The null card is immutable
+                return;
+            }
+
+            switch (relation)
+            {
+                case CARD_INTERFACE.NEXT:
+                    this.updateLinkedCard(CARD_INTERFACE.NEXT, other.getAbsoluteID());
+                    other.updateLinkedCard(CARD_INTERFACE.PREVIOUS, this.getAbsoluteID());
+                    if (getDirectionToOrigin().Equals(DIRECTION_TO_ORIGIN.NEXT)
+                        || getDirectionToOrigin().Equals(DIRECTION_TO_ORIGIN.PREVIOUS))
+                    {
+                        other.setDirectionToOrigin(getDirectionToOrigin());
+                    }
+                    else if (getDirectionToOrigin().Equals(DIRECTION_TO_ORIGIN.IS_ORIGIN))
+                    {
+                        other.setDirectionToOrigin(DIRECTION_TO_ORIGIN.PREVIOUS);
+                    }
+                    break;
+                case CARD_INTERFACE.PREVIOUS:
+                    this.updateLinkedCard(CARD_INTERFACE.PREVIOUS, other.getAbsoluteID());
+                    other.updateLinkedCard(CARD_INTERFACE.NEXT, this.getAbsoluteID());
+                    if (getDirectionToOrigin().Equals(DIRECTION_TO_ORIGIN.NEXT) 
+                        || getDirectionToOrigin().Equals(DIRECTION_TO_ORIGIN.PREVIOUS))
+                    {
+                        other.setDirectionToOrigin(getDirectionToOrigin());
+                    }
+                    else if (getDirectionToOrigin().Equals(DIRECTION_TO_ORIGIN.IS_ORIGIN))
+                    {
+                        other.setDirectionToOrigin(DIRECTION_TO_ORIGIN.NEXT);
+                    }
+                    break;
+                case CARD_INTERFACE.WHY:
+                    this.updateLinkedCard(CARD_INTERFACE.WHY, other.getAbsoluteID());
+                    other.updateLinkedCard(CARD_INTERFACE.RETURN, this.getAbsoluteID());
+                    other.setDirectionToOrigin(DIRECTION_TO_ORIGIN.RETURN);
+                    other.setLevel(this.getLevel() + 1);
+                    break;
+                case CARD_INTERFACE.HOW:
+                    this.updateLinkedCard(CARD_INTERFACE.HOW, other.getAbsoluteID());
+                    other.updateLinkedCard(CARD_INTERFACE.RETURN, this.getAbsoluteID());
+                    other.setDirectionToOrigin(DIRECTION_TO_ORIGIN.RETURN);
+                    other.setLevel(this.getLevel() + 1);
+                    break;
+
+                case CARD_INTERFACE.RETURN:
+                    throw new Exception("Illegal relation modifier was specified : RETURN. Unable to determine forward relation.");
+                default:
+                    throw new Exception("Illegal relation modifier was specified: " + relation.ToString());
+            }
+        }
+        private void setCardType(CARD_TYPE toSet)
+        {
+            cardType = toSet;
+        }
+        private CARD_TYPE getCardType()
+        {
+            return cardType;
+        }
+        private void setLines(int toSet)
+        {
+            lines = toSet;
+        }
+        private int getLines()
+        {
+            return lines;
+        }
+
+        public override string ToString()
+        {
+            string toReturn = "";
+            toReturn += INFOCARD_HEADER_TEXT + "\n";
+            toReturn += getAbsoluteID() + "\n";
+            toReturn += (int)getCardType() + "\n";
+            toReturn += (int)getDirectionToOrigin() + "\n";
+            toReturn += getLevel().ToString() + "\n";
+
+            toReturn += getTimeCreated().ToString() + "\n";
+            toReturn += getTimeEdited().ToString() + "\n";
+            toReturn += getTitle() + "\n";
+            
+            toReturn += getLines().ToString() + "\n";
+            toReturn += getText() + "\n";
+            
+            toReturn += linkedCards.Count.ToString() + "\n";
+            foreach (string every in linkedCards)
+            {
+                toReturn += every + "\n";
+            }
+
+            return toReturn;
+        }
+        private void FromFile(System.IO.StreamReader reader)
+        {
+            if (reader.ReadLine().Equals(INFOCARD_HEADER_TEXT))
+            {
+                Console.WriteLine("Valid data");
+            }
+            setAbsoluteID(reader.ReadLine());
+            setCardType((CARD_TYPE)int.Parse(reader.ReadLine()));
+            setDirectionToOrigin((DIRECTION_TO_ORIGIN)int.Parse(reader.ReadLine()));
+            setLevel(int.Parse(reader.ReadLine()));
+
+            setTimeCreated(DateTime.Parse(reader.ReadLine()));
+            setTimeEdited(DateTime.Parse(reader.ReadLine()));
+            setTitle(reader.ReadLine());
+
+            //Text body
+            int linesOfText = int.Parse(reader.ReadLine());
+            bool firstLine = true;
+            string ofText = "";
+            for (int i = 0; i < linesOfText; i++)
+            {
+                if (!firstLine)
+                {
+                    ofText += "\n";
+                }
+                else
+                {
+                    firstLine = false;
+                }
+                ofText += reader.ReadLine();
+            }
+            setText(ofText);
+
+            //Linked cards
+            initializeLinkedCards();
+            linesOfText = int.Parse(reader.ReadLine());
+            ofText = "";
+            for (int i = 0; i < linesOfText; i++)
+            {
+                setLinkedCard(i, reader.ReadLine());
+            }
+
+        }
+
+    }
+}
